3aab80ba65df3466d7bd6c7a6571d5ea8fb055ca
yuri <yuri@debian>
2026-05-07 09:55:03 +0300

    feat: transition drone movement from direct transform manipulation to physics-based Rigidbody forces

--- Assets/Scripts/Drone/DroneFollowScript.cs
+++ Assets/Scripts/Drone/DroneFollowScript.cs
@@ -7,11 +7,14 @@ public class DroneFollowScript : MonoBehaviour
 
     private Vector3 velocity = Vector3.zero;
     private Vector3 acceleration = Vector3.zero;
+    private Rigidbody rb;
+    private Vector3 liftForce = Vector3.zero; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        liftForce = Vector3.up * 9.81f * rb.mass;
     }
 
     // Update is called once per frame
@@ -24,13 +27,14 @@ public class DroneFollowScript : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
-
         this.acceleration = directionToPlayer * this.maxSpeed;
 
-        this.velocity += this.acceleration * Time.fixedDeltaTime;
-        this.velocity = Vector3.ClampMagnitude(this.velocity, this.maxSpeed);
+        rb.AddForce(this.acceleration * rb.mass + liftForce);
 
-        transform.position += this.velocity * Time.fixedDeltaTime;;
+        if (rb.linearVelocity.magnitude > maxSpeed)
+        {
+            rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+        }
     }
 
     void OnCollisionEnter(Collision collision)
@@ -38,7 +42,7 @@ public class DroneFollowScript : MonoBehaviour
         if (collision.gameObject == player)
         {
             Destroy(collision.gameObject);
-            Destroy(gameObject); 
+            Destroy(gameObject);
         }
         else if (collision.gameObject.CompareTag("Ground"))
         {
